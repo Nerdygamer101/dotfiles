db7182ba564199eaaea2f282afb1b06839b07fe7
Keanen Lovgren <kboeholtlovgren@A18165.local>
2025-12-17 16:08:02 -0800

    Remove command duration in starship

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Enable catppuccin
+require("catppuccin").setup()
+
+-- Set catppuccin colorscheme
+vim.cmd.colorscheme("catppuccin-mocha")
